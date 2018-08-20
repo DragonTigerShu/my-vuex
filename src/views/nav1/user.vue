c30a4818282a8fbd1152a00c5bd5f9d6c38bc5d3
DragonTigerShu <shu_lh@supersoco.com>
2018-08-20 17:24:56 +0800

    update

---
+++ src/views/nav1/user.vue
@@ -0,0 +1,11 @@
+<template>
+    <p class="page-container">user</p>
+</template>
+
+<style lang="less" scoped>
+    .page-container {
+        font-size: 20px;
+        text-align: center;
+        color: rgb(192, 204, 218);
+    }
+</style>
---
+++ src/views/nav2/Page5.vue
@@ -0,0 +1,11 @@
+<template>
+    <p class="page-container">page5</p>
+</template>
+
+<style lang="less" scoped>
+    .page-container {
+        font-size: 20px;
+        text-align: center;
+        color: rgb(192, 204, 218);
+    }
+</style>
